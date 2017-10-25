9fc3904922fa3469a5be55e18c740aa46ae8f3a1
Jean-Kenned <jeanjk99@hotmail.com>
2017-10-25 17:08:45 -0200

    Add files via upload

---
+++ prova1/src/questao1/CasaB.java
@@ -0,0 +1,43 @@
+package questao1;
+
+public class CasaB {
+
+	private Sala s;
+    private Banheiro b;
+	private Cozinha c;
+	private Quarto q[] = new Quarto[3];
+	private int metrosQuintal;
+	public Sala getS() {
+		return s;
+	}
+	public void setS(Sala s) {
+		this.s = s;
+	}
+	public Banheiro getB() {
+		return b;
+	}
+	public void setB(Banheiro b) {
+		this.b = b;
+	}
+	public Cozinha getC() {
+		return c;
+	}
+	public void setC(Cozinha c) {
+		this.c = c;
+	}
+	public Quarto[] getQ() {
+		return q;
+	}
+	public void setQ(Quarto[] q) {
+		this.q = q;
+	}
+	public int getMetrosQuintal() {
+		return metrosQuintal;
+	}
+	public void setMetrosQuintal(int metrosQuintal) {
+		this.metrosQuintal = metrosQuintal;
+	}
+	
+	
+	
+}
